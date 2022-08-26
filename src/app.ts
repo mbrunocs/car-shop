c941e4d7396647299d8afa8f6fc312363b19adc2
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-08-26 19:15:58 -0300

    dev: apply json

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import routes from './routers/indexRoutes';
 
 const app = express();
+app.use(express.json());
 app.get('/', (_req, res) => res.json({ ok: true }));
 app.use(routes);
 
